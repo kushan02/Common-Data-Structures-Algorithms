f446bfae29ee4254eb64f1a760643a002d2d6a23
Kushan Mehta <kushan.mehta02@gmail.com>
2019-10-13 11:22:07 +0530

    add python code for power implementation for large numbers

--- power.cpp
+++ power.cpp
@@ -22,6 +22,20 @@ int main() {
 
     cout << (bool)(a==b); // verify correctness of our implementation
 
+    // pro tip: switch to python if you think answer will overflow
+    /*
+    def power(a,n):
+    if a == 1 or n == 0:
+        return 1
+    r = power(a, n//2)
+    if n%2 == 0:
+        return r*r
+    else:
+        return r*a*r
+
+print( power(5,34) )
+
+     */
 
     return 0;
 }
